b408953f70dca74112c0e5229a990ea48b2e3116
fengxiangxin <227306865@qq.com>
2020-11-29 00:42:21 +0800

    feat：进度条功能

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,10 @@
 /* axios拦截器 */
 import axios from "axios";
+/* 引入进度条 */
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
-/*  */
+/* instance功能与axios类似，但是不完全相同 */
 const instance = axios.create({
   baseURL: "/api",
   headers: {},
@@ -10,6 +13,8 @@ const instance = axios.create({
 /* 设置请求拦截器 */
 instance.interceptors.request.use(
   (config) => {
+    /* 开始进度条 */
+    NProgress.start();
     /**
      * config是请求的配置对象
      * 包含请求方式，请求地址，请求参数等
@@ -23,6 +28,8 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   /* 响应成功的http状态码为2xx */
   (response) => {
+    /* 结束进度条 */
+    NProgress.done();
     /**
      * 根据返回的数据中的code值判断是否请求到正确的数据
      * 只有当code===200时才说明请求到正确的数据
@@ -31,12 +38,14 @@ instance.interceptors.response.use(
       /* 将数据作为promise的结果值返回 */
       return response.data.data;
     }
-    console.log(response.data);
+    // console.log(response.data);
     /* code!==200说明出现错误，没有请求到数据，返回失败的promise */
     return Promise.reject(response.data.message);
   },
   /* 响应失败 */
   (error) => {
+    /* 结束进度条 */
+    NProgress.done();
     /* 返回失败的promise，值为错误信息 */
     return Promise.reject(error.message);
   }
